1d9500adeb0c5b7fda3d6d8b1c8baa42ba4c89dd
Joel DeVenney <jfreak53@gmail.com>
2021-07-07 14:11:32 -0400

    Added more code for the thingspeak api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 const path=require('path');
 var http = require('http');
+// const sensor = require('ds18b20-raspi');
 var config = require('./config');
 var temp;
 
@@ -11,18 +12,27 @@ app.set('view engine', 'pug');
 
 app.use(express.static('assets'));
 
-/*http.get({
-  host: 'api.thingspeak.com',
-  port: 443,
-  path: '/update?api_key=' + config.thingspeak + '&field1=' + temp
-}, function ( res ) {
-  // console.log("Got response: " + res.statusCode);
-});*/
+if(config.thingspeak > '') {
+	/*setInterval(function () {
+		temp = sensor.readSimpleF();
+
+		if(temp > '') {
+			http.get({
+				host: 'api.thingspeak.com',
+				port: 443,
+				path: '/update?api_key=' + config.thingspeak + '&field1=' + temp
+			}, function ( res ) {
+				// console.log("Got response: " + res.statusCode);
+			});
+		}
+	}, 1000 * 60 * config.thingspeakcheck);*/
+}
 
 app.get('/', function ( req, res ) {
+	// temp = sensor.readSimpleF();
 	res.render('index', { temp: 28 + ' &deg;F' });
 });
 
 app.listen(port, () => {
-  console.log('Pool Temp listening at http://localhost:'+port);
+  console.log('Pool Temp listening at port '+port);
 });
